2c0ac1541c1a8805fda718db0ec998b97134dc99
CarlosSenra <crsbsenra@gmail.com>
2025-05-08 10:16:03 -0300

    first commit

---
+++ docs/docs/index.md
@@ -0,0 +1,10 @@
+# ml-project documentation!
+
+## Description
+
+Meu projeto para por um modelo de machine learning em deploy
+
+## Commands
+
+The Makefile contains the central entry points for common tasks related to this project.
+
---
+++ src/app/api.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import os
+
+import pickle
+
+colunas = ['tamanho', 'ano', 'garagem']
+modelo = pickle.load(open('../../models/modelo.sav','rb'))
+
+app = Flask('my_app')
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def home():
+    return "Minha primeira API."
+
+@app.route('/sentimento/<frase>')
+@basic_auth.required
+def senntimento(frase):
+    tb = TextBlob(frase)
+    polaridade = tb.sentiment.polarity
+    return f"Polaridade : {polaridade}"
+
+@app.route('/cotacao/', methods=['POST'])
+@basic_auth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    pred = modelo.predict([dados_input])
+    return jsonify(pred=pred[0])
+
+
+app.run(debug=True, host="0.0.0.0")
+
